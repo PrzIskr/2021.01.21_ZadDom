896f733f7b3ea6654882d57a3ad40a31de37b4a3
PrzIskr <przemyslaw.iskra@student.up.krakow.pl>
2021-01-21 21:51:17 +0100

    dokończone

--- zad8_Przemysław_Iskra.py
+++ zad8_Przemysław_Iskra.py
@@ -1,1 +1,8 @@
-pliczek aaa
+txt_file = open('plik01.txt', 'w+')
+txt_file.write('w pierwszej ćwiartce wszystkie są dodatnie,'
+               '\nw drugiej tylko sinus,'
+               '\nw trzeciej tangens i cotangens,'
+               '\na w czwartej cosinus')
+txt_file = open('plik01.txt', 'r')
+print(txt_file.read())
+txt_file.close()
