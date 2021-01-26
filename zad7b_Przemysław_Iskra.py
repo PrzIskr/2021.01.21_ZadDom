05d72eca157b41f77bebe1e6bf79b5c67830d3fa
PrzIskr <przemyslaw.iskra@student.up.krakow.pl>
2021-01-26 23:56:26 +0100

    7b

--- zad7b_Przemysław_Iskra.py
+++ zad7b_Przemysław_Iskra.py
@@ -7,7 +7,7 @@ def person_print(name, last_name, age, *others):
         others_str += arg + ' '
     print(formatted_data + others_str)
 
-person_print('Przemysław', 'Iskra', "kraj: Polska", "miasto: Kraków", 21)
+person_print('Przemysław', 'Iskra', 21, "kraj: Polska", "miasto: Kraków")
 
 # Odp: Nie można wywołać tej funkcji bez wprowadzenia zmian
 # Aby było to możliwe argument args/kwargs musi się znajdować na końcu deklaracji funkcji
